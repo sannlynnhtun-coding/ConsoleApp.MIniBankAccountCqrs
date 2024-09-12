ab9aa30b475a5eda84824f243831944255c883e8
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-12 10:24:38 +0630

    feat: add logic

---
+++ ConsoleApp.MIniBankAccountCqrs/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿// Global using directives
+
+global using ConsoleApp.MIniBankAccountCqrs.EventStore;
+global using ConsoleApp.MIniBankAccountCqrs.Models;
+global using ConsoleApp.MIniBankAccountCqrs.Services;
--- ConsoleApp.MIniBankAccountCqrs/Program.cs
+++ ConsoleApp.MIniBankAccountCqrs/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Mini Bank Account CQRS");
+
+// Create event store (shared by both services)
+var eventStore = new EventStore();
+
+// Create the command and query services
+var commandService = new BankAccountCommandService(eventStore);
+var queryService = new BankAccountQueryService(eventStore);
+
+// Perform some operations
+commandService.DepositMoney(1000);
+commandService.WithdrawMoney(200);
+commandService.DepositMoney(500);
+
+// Query the current balance
+var balance = queryService.GetBalance();
+Console.WriteLine($"Current Balance: {balance}"); // Output: Current Balance: 1300
+Console.ReadKey();
