1c977f24744c5567e673a686541f7f324bbb1abd
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-12 10:23:52 +0630

    feat: add services

---
+++ ConsoleApp.MIniBankAccountCqrs/Services/BankAccountCommandService.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp.MIniBankAccountCqrs.Services;
+
+public class BankAccountCommandService
+{
+	private readonly EventStore.EventStore _eventStore;
+
+	public BankAccountCommandService(EventStore.EventStore eventStore)
+	{
+		_eventStore = eventStore;
+	}
+
+	public void DepositMoney(decimal amount)
+	{
+		var depositEvent = new DepositEvent(amount);
+		_eventStore.SaveEvent(depositEvent);
+	}
+
+	public void WithdrawMoney(decimal amount)
+	{
+		var withdrawalEvent = new WithdrawalEvent(amount);
+		_eventStore.SaveEvent(withdrawalEvent);
+	}
+}
---
+++ ConsoleApp.MIniBankAccountCqrs/Services/BankAccountQueryService.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp.MIniBankAccountCqrs.Services;
+
+public class BankAccountQueryService
+{
+	private readonly EventStore.EventStore _eventStore;
+
+	public BankAccountQueryService(EventStore.EventStore eventStore)
+	{
+		_eventStore = eventStore;
+	}
+
+	public decimal GetBalance()
+	{
+		decimal balance = 0;
+
+		foreach (var bankEvent in _eventStore.GetEvents())
+		{
+			if (bankEvent is DepositEvent)
+				balance += bankEvent.Amount;
+			else if (bankEvent is WithdrawalEvent)
+				balance -= bankEvent.Amount;
+		}
+
+		return balance;
+	}
+}
