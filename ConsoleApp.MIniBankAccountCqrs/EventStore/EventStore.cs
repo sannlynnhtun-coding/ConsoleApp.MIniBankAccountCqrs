c3b949cd1f3334e02afcdc034b8fc2e9f34c64ab
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-12 10:24:03 +0630

    feat: add event store

---
+++ ConsoleApp.MIniBankAccountCqrs/EventStore/EventStore.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.MIniBankAccountCqrs.EventStore;
+
+public class EventStore
+{
+	private readonly List<BankAccountEvent> _events = new List<BankAccountEvent>();
+
+	public void SaveEvent(BankAccountEvent bankEvent)
+	{
+		_events.Add(bankEvent);
+	}
+
+	public List<BankAccountEvent> GetEvents()
+	{
+		return _events;
+	}
+}
